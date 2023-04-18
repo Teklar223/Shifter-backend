21574349e7355daa0c51a97e0074f11cb183aa2d
Yonatan Ratner <yonaratner@gmail.com>
2023-04-18 15:04:18 +0300

    basic POST and GET methods for all endpoints

--- base_app/bl_domains/role_domain.py
+++ base_app/bl_domains/role_domain.py
@@ -1,16 +1,28 @@
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from ..constants import role_id
-from ..models import Role,CompanyRoleRequisite
+from ..constants import role_id, company_id, error_id
+from ..models import Role, CompanyRoleRequisite
 from ..serializers import RoleSerializer, RoleRequisiteSerializer
 
 ''' Role  '''
 
 def RoleGet(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if role_id in kwargs:
+        role = Role.objects.get(id = kwargs.get(role_id, error_id), company_id = kwargs.get(company_id, error_id))
+        serializer = RoleSerializer(role, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        role = Role.objects.all()
+        serializer = RoleSerializer(role, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 def RolePost(request, *args, **kwargs) -> JsonResponse:
-    pass
+    data = JSONParser().parse(request)
+    serializer = RoleSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 def RolePut(request, *args, **kwargs) -> JsonResponse:
     pass
@@ -20,10 +32,22 @@ def RoleDelete(request, *args, **kwargs) -> JsonResponse:
 
 ''' Role Requisite '''
 def RoleRequisiteGet(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if role_id in kwargs:
+        roleReq = CompanyRoleRequisite.objects.get(role_id = kwargs.get(role_id, error_id), company_id= kwargs.get(company_id, error_id))
+        serializer = RoleRequisiteSerializer(roleReq, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        roleReq = CompanyRoleRequisite.objects.all()
+        serializer = RoleRequisiteSerializer(roleReq, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 def RoleRequisitePost(request, *args, **kwargs) -> JsonResponse:
-    pass
+    data = JSONParser().parse(request)
+    serializer = RoleRequisiteSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 def RoleRequisitePut(request, *args, **kwargs) -> JsonResponse:
     pass
--- base_app/bl_domains/team_domain.py
+++ base_app/bl_domains/team_domain.py
@@ -1,16 +1,29 @@
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from ..constants import team_id
+from ..constants import team_id, employee_id, company_id, role_id, error_id
 from ..models import Team,TeamEmployee,TeamRoleRequisites
 from ..serializers import TeamSerializer, TeamEmployeeSerializer, TeamRequisiteSerializer
+from ..util import prints_args_kwargs # TODO: rm
 
 ''' Team  '''
 
 def TeamGet(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if team_id in kwargs:
+        team = Team.objects.get(id = kwargs.get(team_id, error_id), company_id= kwargs.get(company_id, error_id))
+        serializer = TeamSerializer(team, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        team = Team.objects.all()
+        serializer = TeamSerializer(team, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 def TeamPost(request, *args, **kwargs) -> JsonResponse:
-    pass
+    data = JSONParser().parse(request)
+    serializer = TeamSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 def TeamPut(request, *args, **kwargs) -> JsonResponse:
     pass
@@ -21,10 +34,22 @@ def TeamDelete(request, *args, **kwargs) -> JsonResponse:
 ''' Team Requisite '''
 
 def TeamRequisiteGet(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if team_id in kwargs:
+        teamReq = TeamRoleRequisites.objects.get(team_id = kwargs.get(team_id, error_id), role_id= request.GET.get(role_id))
+        serializer = TeamRequisiteSerializer(teamReq, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        teamReq = TeamRoleRequisites.objects.all()
+        serializer = TeamRequisiteSerializer(teamReq, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 def TeamRequisitePost(request, *args, **kwargs) -> JsonResponse:
-    pass
+    data = JSONParser().parse(request)
+    serializer = TeamRequisiteSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 def TeamRequisitePut(request, *args, **kwargs) -> JsonResponse:
     pass
@@ -35,10 +60,23 @@ def TeamRequisiteDelete(request, *args, **kwargs) -> JsonResponse:
 ''' Team Employee '''
 
 def TeamEmployeeGet(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if team_id in kwargs:
+        teamEmp = TeamEmployee.objects.get(team_id = kwargs.get(team_id, error_id), employee_id = request.GET.get(employee_id))
+        serializer = TeamEmployeeSerializer(teamEmp, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        teamEmp = TeamEmployee.objects.all()
+        serializer = TeamEmployeeSerializer(teamEmp, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
 
 def TeamEmployeePost(request, *args, **kwargs) -> JsonResponse:
-    pass
+    data = JSONParser().parse(request)
+    serializer = TeamEmployeeSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 def TeamEmployeePut(request, *args, **kwargs) -> JsonResponse:
     pass
