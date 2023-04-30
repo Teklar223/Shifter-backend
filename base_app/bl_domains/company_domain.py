8d7ba6c5e1d826198eef499a234c6a098cc12a2c
Teklar223 <yonaratner@gmail.com>
2023-04-30 14:19:26 +0300

    +PUT and DELETE request for Company (all other objects todo)

--- base_app/bl_domains/company_domain.py
+++ base_app/bl_domains/company_domain.py
@@ -4,10 +4,13 @@ from ..constants import company_id # id's come from heres
 from ..models import Company
 from ..serializers import CompanySerializer
 
+# TODO: You called this URL via PUT, but the URL doesn't end in a slash and you have APPEND_SLASH set. Django can't redirect to the slash URL while maintaining PUT data. Change your form to point to 127.0.0.1:8000/api/1/ (note the trailing slash), or set APPEND_SLASH=False in your Django settings.
+
 def CompanyGet(request, *args, **kwargs) -> JsonResponse:
-    #TODO: handle 301 (resource not found)
+    #TODO: handle 301 (resource not found) *and then do so for all requests
     if company_id in kwargs:
-        company = Company.objects.get(id = kwargs.get(company_id))
+        id = kwargs.get(company_id)
+        company = Company.objects.get(id = id)
         serializer = CompanySerializer(company, many=False)
         return JsonResponse(serializer.data, safe=False)
     else:
@@ -16,6 +19,7 @@ def CompanyGet(request, *args, **kwargs) -> JsonResponse:
         return JsonResponse(serializer.data, safe=False)
     
 def CompanyPost(request, *args, **kwargs) -> JsonResponse:
+    # TODO: add a 'Consistency' check
     data = JSONParser().parse(request)
     serializer = CompanySerializer(data=data)
     if serializer.is_valid():
@@ -24,7 +28,27 @@ def CompanyPost(request, *args, **kwargs) -> JsonResponse:
     return JsonResponse(serializer.errors, status=400)
 
 def CompanyPut(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if company_id in kwargs:
+        data = JSONParser().parse(request)
+        id = kwargs.get(company_id)
+        company = Company.objects.get(id = id)
+        serializer = CompanySerializer(company,data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            response = serializer.data
+            return JsonResponse(response, status=202)
+        else:
+            return JsonResponse(serializer.errors, status=500)
+    else:
+        return JsonResponse("Forbidenn", status=401)
 
 def CompanyDelete(request, *args, **kwargs) -> JsonResponse:
-    pass
+    if company_id in kwargs:
+        # data = JSONParser().parse(request)
+        id = kwargs.get(company_id)
+        company = Company.objects.filter(id = id)
+        company.delete()
+        response = "Successfully deleted 'Company' with ID="+id
+        return JsonResponse(response, status=200,safe=False)
+    else:
+        return JsonResponse("Forbidden", status=401)
