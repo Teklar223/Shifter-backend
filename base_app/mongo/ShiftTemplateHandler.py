470b24f29134c2990cb1ac02ecf0366baa7331f4
Ofri Tavor <ofritavor55@gmail.com>
2023-06-23 19:09:32 +0300

    fixed issue with shift template update

--- base_app/mongo/ShiftTemplateHandler.py
+++ base_app/mongo/ShiftTemplateHandler.py
@@ -65,11 +65,12 @@ class Shift_Template_Handler(CollectionHandler):
         start = shift_template.get_start_hour()
         end = shift_template.get_end_hour()
         needed_roles = shift_template.get_needed_roles()
+        name = shift_template.get_template_name()
         self.collection.find_one_and_update(
             {Template_id: template_id},
             {"$set":
                  {f"{Template_id}": template_id, f"{Team_id}": team_id, f"{Start_hour}": start, f"{End_hour}": end,
-                  f"{NeededRoles}": needed_roles}
+                  f"{NeededRoles}": needed_roles, "TemplateName": name}
              }, upsert=True
         )
 
